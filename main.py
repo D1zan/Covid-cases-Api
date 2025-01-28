95f75cc7746f9e84da8ed88c6dd5d36d175be35f
sarah <student.189241@worcesterschools.net>
2025-01-28 09:36:34 -0500

    1/28/25

--- main.py
+++ main.py
@@ -15,63 +15,19 @@ class Covid:
         print(f"Recoveries: {self.recovered}")
         print(f"Countries affected: {self.affectedCountries}")
 
+# outside of class##################
 #Getting the data, get request
-    def fetch_covid_us(covid_data):
-        url = f"https://disease.sh/v3/covid-19/historical/us"
-        response = requests.get(url)
+def fetch_covid_info(country):
+    url = f"https://disease.sh/v3/covid-19/historical/{country}"
+    response = requests.get(url)
 
-        if response.status_code == 200:
-            covid_data = response.json()
-            return covid_data
-        else:
-            print(f"Sorry, that is not related to my information knowledge")
-            return None
-        
-    def fetch_covid_canada(covid_data):
-        url = f"https://disease.sh/v3/covid-19/historical/canada"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            covid_data = response.json()
-            return covid_data
-        else:
-            print(f"Sorry, that is not related to my information knowledge")
-            return None
-        
-    def fetch_covid_brazil(covid_data):
-        url = f"https://disease.sh/v3/covid-19/historical/brazil"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            covid_data = response.json()
-            return covid_data
-        else:
-            print(f"Sorry, that is not related to my information knowledge")
-            return None
-        
-    def fetch_covid_ghana(covid_data):
-        url = f"https://disease.sh/v3/covid-19/historical/ghana"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            covid_data = response.json()
-            return covid_data
-        else:
-            print(f"Sorry, that is not related to my information knowledge")
-            return None
-        
-    def fetch_covid_portugal(covid_data):
-        url = f"https://disease.sh/v3/covid-19/historical/portugal"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            covid_data = response.json()
-            return covid_data
-        else:
-            print(f"Sorry, that is not related to my information knowledge")
-            return None
-            
-
+    if response.status_code == 200:
+        covid_data = response.json()
+        return covid_data
+    else:
+        print(f"Sorry, that is not related to my information knowledge")
+        return None
+   
 #Creating an object for the class
 def covid_info(covid_json):
     covid = Covid(
@@ -83,33 +39,27 @@ def covid_info(covid_json):
 
 
 #User interaction
-print()
-print("Hi, Welcome to Covid Information center")
-print("These are the countries with the available information to you: ")
-print("Brazil, " "\n" "United States " "\n" "Canada, " "\n" "Ghana, " "\n" "Portugal." )
-print()
 
 
-if user_input == "Brazil":
-    print(covid_info)
-elif "Portugal":
-    print(covid_info(covid_json))
-elif "Canada":
-    print(covid_info)
-elif "United States":
-    print(covid_info)
-elif "Portugal":
-    print(covid_info)
-else: 
-    "Please pick from the list given"
-
-#Ask user to pick a country
-country = input("Which country would you like to ask about?: ")
-
-
+while True:
+    print()
+    print("Hi, Welcome to Covid Information center")
+    print("These are the countries with the available information to you: ")
+    user_input = input("Brazil,\nUnited States \nCanada,\nGhana,\nPortugal ")
+    country = input("Which country would you like to ask about?: ")
+    #print(country)
+    #print(fetch_covid_info(country))
 #Ask user to pick a question 
-
-questions = int(input("(1) How many cases of covid in total? \n (2)How many people have died from covid? \n (3)How many people in total have recovered? " ))
-
+    questions = int(input("(1) How many cases of covid in total? \n(2)How many people have died from covid? \n(3)How many people in total have recovered? " ))
+    print("Please pick numbers 1-3")
+    if questions == "1":
+        print(fetch_covid_info(covid_info("cases")))
+    elif questions == "2":
+        print(fetch_covid_info(covid_info("todayDeaths")))
+    elif questions == "3": 
+        print(fetch_covid_info(covid_info("recovered")))
 
 # print the country + the answer to the picked question
+    keep_going = input("Would you like to ask about another country? (y/n): ").lower().strip()
+    if keep_going == "n":
+        break
